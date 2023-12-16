f9f16fb4f094cb8eee13f6da3253b9841c9a2611
Marco <marco.pertegal.prieto@gmail.com>
2023-12-16 13:25:55 +0100

    Cors config added

--- springboot-rest-jwt/src/main/java/net/openwebinars/springboot/restjwt/config/WebConfig.java
+++ springboot-rest-jwt/src/main/java/net/openwebinars/springboot/restjwt/config/WebConfig.java
@@ -9,13 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig
 {
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
+    public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("*")
                         .allowedOrigins("*");
             }
         };
